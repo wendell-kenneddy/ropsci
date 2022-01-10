8571832b4539c4bdf04db244d4bf370e74e51d2c
Wendell Kenneddy <wkenneddy505@gmail.com>
2022-01-10 16:18:13 -0300

    chore(tailwind_config): add more color values

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,10 @@ module.exports = {
       colors: {
         'dark-text': 'hsl(229, 25%, 31%)',
         score: 'hsl(229, 64%, 46%)',
-        'header-outline': 'hsl(217, 16%, 45%)'
+        'header-outline': 'hsl(217, 16%, 45%)',
+        'rock-border': '#dc2e4e',
+        'scissors-border': '#ec9e0e',
+        'paper-border': '#4865f4'
       }
     }
   },
