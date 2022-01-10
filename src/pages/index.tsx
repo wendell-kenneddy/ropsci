19f645593c7b3b1fd903edc0004545f96976f2c5
Wendell Kenneddy <wkenneddy505@gmail.com>
2022-01-10 16:22:07 -0300

    feat(home_page): update content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
+import { FirstStep } from '../components/FirstStep';
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline ">Hello, world!</h1>
-    </div>
+    <main role="main" className="my-12">
+      <FirstStep />
+    </main>
   );
 }
