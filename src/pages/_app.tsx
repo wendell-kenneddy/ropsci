8c885950d35e8510b56def43aac6f1d50c65cfc4
Wendell Kenneddy <wkenneddy505@gmail.com>
2022-01-11 15:51:26 -0300

    feat(_app): add footer component to layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 
 import { Header } from '../components/Header';
+import { Footer } from '../components/Footer';
 
 import '../styles/globals.css';
 
@@ -10,6 +11,8 @@ export default function App({ Component, pageProps }: AppProps) {
       <Header />
 
       <Component {...pageProps} />
+
+      <Footer />
     </>
   );
 }
