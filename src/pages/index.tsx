e83842a494df004861dfde8b152bc6ab5ed1f365
Wendell Kenneddy <wkenneddy505@gmail.com>
2022-01-14 13:59:56 -0300

    feat(home_page): add meta tags for better SEO

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import Head from 'next/head';
+
 import { useGame } from '../hooks/useGame';
 
 import { FirstStep } from '../components/FirstStep';
@@ -7,8 +9,30 @@ export default function Home() {
   const { step } = useGame();
 
   return (
-    <main role="main" className="my-12">
-      {step === 'initial' ? <FirstStep /> : <FinalStep />}
-    </main>
+    <>
+      <Head>
+        <meta name="author" content="Wendell Kenneddy" />
+        <meta name="description" content="A Rock, Paper, Scissors app." />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Ropsci" />
+        <meta property="og:site_name" content="Ropsci" />
+        <meta
+          property="og:description"
+          content="A Rock, Paper, Scissors app."
+        />
+        <meta property="og:locale" content="pt-BR" />
+        <meta property="twitter:card" content="summary" />
+        <meta property="twitter:title" content="Ropsci" />
+        <meta
+          property="twitter:description"
+          content="A Rock, Paper, Scissors app."
+        />
+        <title>Ropsci</title>
+      </Head>
+
+      <main role="main" className="my-12">
+        {step === 'initial' ? <FirstStep /> : <FinalStep />}
+      </main>
+    </>
   );
 }
