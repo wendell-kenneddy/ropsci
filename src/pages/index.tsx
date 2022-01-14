591400238a33d1d0d51f034cd63b47f671dd538e
Wendell Kenneddy <wkenneddy505@gmail.com>
2022-01-14 13:15:24 -0300

    feat(home_page): update page to use the game context

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
+import { useGame } from '../hooks/useGame';
+
 import { FirstStep } from '../components/FirstStep';
+import { FinalStep } from '../components/FinalStep';
 
 export default function Home() {
+  const { step } = useGame();
+
   return (
     <main role="main" className="my-12">
-      <FirstStep />
+      {step === 'initial' ? <FirstStep /> : <FinalStep />}
     </main>
   );
 }
