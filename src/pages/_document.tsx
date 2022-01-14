bd7096a4629690b7e63b1f96075911f2036d9654
Wendell Kenneddy <wkenneddy505@gmail.com>
2022-01-14 13:59:30 -0300

    feat(_document): add link to icons and manifest

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,12 +3,17 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="pt-br" className="scroll-smooth">
+      <Html lang="en" className="scroll-smooth">
         <Head>
           <link
             href="https://fonts.googleapis.com/css2?family=Barlow+Semi+Condensed:wght@600;700&display=swap"
             rel="stylesheet"
           />
+          <link rel="icon" href="/favicon.ico" sizes="any" />
+          <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+          <meta name="theme-color" content="#1f3756" />
+          <link rel="manifest" href="/manifest.json" />
         </Head>
         <body className="w-full h-screen bg-[#1f3756]">
           <Main />
